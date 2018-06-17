a72178c602ee94af59051e4d299d932b32e5fee7
Zdenek Sotolar <zdenek.sotolar@peckadesign.cz>
2018-06-17 19:33:22 +0200

    New route /kokot

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,10 +6,18 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+    
+    router.get("kokot", use: {(req: Request) in
+        return Grason(name: "Zdenek")
+    })
 
     // Example of configuring a controller
     let todoController = TodoController()
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
+}
+
+struct Grason: Content {
+    let name: String
 }
